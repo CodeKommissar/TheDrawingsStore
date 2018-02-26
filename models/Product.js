630d6cda4e657c475abe4f9983e464796296d361
luisrodriguez <codekommissar@gmail.com>
2018-02-26 15:01:15 -0400

    Create Production Environment

--- models/Product.js
+++ models/Product.js
@@ -4,7 +4,6 @@ const { Schema } = mongoose;
 const productSchema = new Schema({
     imagePath: {type: String, required: true},
     title: {type: String, required: true},
-    description: {type: String, required: true},
     price: {type: Number, required: true},
 });
 
