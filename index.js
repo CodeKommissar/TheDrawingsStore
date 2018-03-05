a4ab20ca424f2de2bf39f7bd56f8df8c8f89c135
luisrodriguez <codekommissar@gmail.com>
2018-03-05 14:31:47 -0400

    Add products api route

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/api/products", (req, res) => {
-    res.send({ hello: "world" })
-});
+require("./routes/productsRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
