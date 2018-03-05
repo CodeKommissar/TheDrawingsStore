424cd153afed055c2f1169f0a23a168b072ea805
luisrodriguez <codekommissar@gmail.com>
2018-03-05 14:41:26 -0400

    Serve products endpoint

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,5 +1,10 @@
+const mongoose = require("mongoose");
+
+const Product = mongoose.model("Product");
+
 module.exports = (app) => {
-    app.get("/api/products", (req, res) => {
-        res.send({ products: "Hello World!" });
+    app.get("/api/products", async (req, res) => {
+        const allProducts = await Product.find({});
+        res.send(allProducts);
     });
 };
